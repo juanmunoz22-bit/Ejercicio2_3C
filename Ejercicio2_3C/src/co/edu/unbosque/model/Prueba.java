75f3ea973b20a237934800627bd19ad94239a95b
kgvservicios <kgvservicios@DESKTOP-N9VOB1S>
2020-05-02 21:37:28 -0500

    b

---
+++ Ejercicio2_3C/src/co/edu/unbosque/model/Prueba.java
@@ -0,0 +1,14 @@
+package co.edu.unbosque.model;
+
+public class Prueba {
+	
+	public void insercion() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
