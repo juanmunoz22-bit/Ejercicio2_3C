f12957f4ad5f4887f6bb065b70903703aea10854
kgvservicios <kgvservicios@DESKTOP-N9VOB1S>
2020-05-02 22:36:37 -0500

    v5

--- Ejercicio2_3C/src/co/edu/unbosque/model/Prueba.java
+++ Ejercicio2_3C/src/co/edu/unbosque/model/Prueba.java
@@ -1,13 +1,63 @@
 package co.edu.unbosque.model;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import co.edu.unbosque.model.persistence.Leer;
+
 public class Prueba {
+
+	private Leer l = new Leer();
 	
 	public void insercion() {
+		
+		int aux, pos;
+		ArrayList<String> letras = new ArrayList<String>();
+		ArrayList<Integer> n = new ArrayList<Integer>();
+		File letra = new File("./src/data/letras");
+		l.leerChar(letra, letras);
+		
+		System.out.println("Arreglo sin ordenar");
+		for (int i = 0; i < letras.size(); i++) {
+			System.out.print(letras.get(i)+" ");
+		}
+		
+		for(int i=0; i<letras.size(); i++) {
+			int asc = letras.get(i).charAt(0);
+			n.add(asc);
+		}
+		
+		
+		System.out.println("\nArreglo ordenado ascendentemente");
+		for(int i = 0; i < n.size()-1; i++) {
+			pos = i;
+			aux = n.get(i);
+			
+			while((pos>0) && (n.get(pos-1)>aux)) {
+				n.set(pos, n.get(pos-1));
+				pos--;
+			}
+			n.set(pos, aux);
+		}
+		
+		for(int i= 0; i<n.size();i++) {
+			int num = n.get(i);
+			char c = (char)(num);
+			System.out.print(c+" ");
+		}
+		System.out.println("\nArreglo ordenado descendentemente");
+		
+		for (int i = n.size()-1; i>=0;i--) {
+			int num = n.get(i);
+			char c = (char)(num);
+			System.out.print(c+" ");
+		}
 		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Prueba p = new Prueba();
+		p.insercion();
 
 	}
 
--- Ejercicio2_3C/src/co/edu/unbosque/model/persistence/Leer.java
+++ Ejercicio2_3C/src/co/edu/unbosque/model/persistence/Leer.java
@@ -4,8 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+
 /**
- * Clase  que se encarga de leer un archivo de texto para hacer diferentes operaciuones visuales
+ * Clase que se encarga de leer un archivo de texto para hacer diferentes
+ * operaciuones visuales
+ * 
  * @author Grupo Votacion
  *
  */
@@ -13,40 +16,67 @@ public class Leer {
 
 	FileReader fr = null;
 	BufferedReader br = null;
-	
+
 	/**
 	 * Metodo que lee una fecha en un archivo plano
-	 * @param fecha Archivo plano  que lee el sistema
+	 * 
+	 * @param fecha    Archivo plano que lee el sistema
 	 * @param burbuja1 Listado de caracteres
 	 */
 	public void leerNum(File numero, ArrayList<Integer> numeros) {
 		try {
-	        // Apertura del fichero y creacion de BufferedReader para poder
-	        // hacer una lectura comoda (disponer del metodo readLine()).
-	        fr = new FileReader (numero);
-	        br = new BufferedReader(fr);
-	        // Lectura del fichero
-	        String linea;
-	        while((linea=br.readLine())!=null) {
-	       	 	numeros.add(Integer.parseInt(linea));
-	           }
-	        
-	        
-	     }
-	     catch(Exception e){
-	        e.printStackTrace();
-	     }finally{
-	         // En el finally cerramos el fichero, para asegurarnos
-	         // que se cierra tanto si todo va bien como si salta 
-	         // una excepcion.
-	         try{                    
-	            if( null != fr ){   
-	               fr.close();     
-	            }                  
-	         }catch (Exception e2){ 
-	            e2.printStackTrace();
-	         }
-	      }
+			// Apertura del fichero y creacion de BufferedReader para poder
+			// hacer una lectura comoda (disponer del metodo readLine()).
+			fr = new FileReader(numero);
+			br = new BufferedReader(fr);
+			// Lectura del fichero
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				numeros.add(Integer.parseInt(linea));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// En el finally cerramos el fichero, para asegurarnos
+			// que se cierra tanto si todo va bien como si salta
+			// una excepcion.
+			try {
+				if (null != fr) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
 	}
-	
+
+	public void leerChar(File numero, ArrayList<String> letras) {
+		try {
+			// Apertura del fichero y creacion de BufferedReader para poder
+			// hacer una lectura comoda (disponer del metodo readLine()).
+			fr = new FileReader(numero);
+			br = new BufferedReader(fr);
+			// Lectura del fichero
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				letras.add(linea);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// En el finally cerramos el fichero, para asegurarnos
+			// que se cierra tanto si todo va bien como si salta
+			// una excepcion.
+			try {
+				if (null != fr) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+
 }
